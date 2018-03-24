37a7dceac689fa0bcba8f3d347348522cbb6f0e6
Alex-H <hjystc001@126.com>
2018-03-24 22:54:36 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# encoding=utf-8
+
+import pandas
+import numpy as np
+import math
+import pymysql
+import matplotlib as plb
+import pandas as pd
+import keras as kr
+
+
+d = { 'Adam': 95, 'Lisa': 85, 'Bart': 59, 'Paul': 74 }
+def func(a,b,f):
+    return f(a)+f(b)
+print(abs)

cb41a026a49ebc18780d1384e54ea00d26c73ca3
Alex-H <hjystc001@126.com>
2018-04-24 20:20:05 +0800

    4.24

--- 滑动窗口算异常值.py
+++ 滑动窗口算异常值.py
@@ -15,6 +15,43 @@ import Apriori
 from scipy.interpolate import lagrange
 
 
+def read_mysql(sql='select * from test.total where transformer like \'省检修分公司南京分部东善桥变重庆ABB变压器有限公司2号主变A相\''):  # 读取数据
+    try:
+        conn = pymysql.connect(host="localhost", user="root", password="123456789", db="test", port=3306,
+                               charset='utf8')
+    except pymysql.err.OperationalError as e:
+        print('Error is ' + str(e))
+    try:
+        engine = sqlalchemy.create_engine('mysql+pymysql://root:123456789@localhost:3306/test')
+    except sqlalchemy.exc.OperationalError as e:
+        print('Error is ' + str(e))
+
+    except sqlalchemy.exc.InternalError as e:
+        print('Error is ' + str(e))
+    try:
+        df = pd.read_sql(sql, con=conn)
+    except pymysql.err.ProgrammingError as e:
+        print('Error is ' + str(e))
+    # print(df.head())
+    conn.close()
+    print(df.head())
+    print('ok')
+    return df
+
+
+def ployinterp_column(s, n, k=5):
+    y = s[list(range(n - k, n)) + list(range(n + 1, n + 1 + k))]
+    y = y[y.notnull()]
+    return lagrange(y.index, list(y))(n)
+
+
+def meaninterp_column(s, n, k=5):
+    y = s[list(range(n - k, n)) + list(range(n + 1, n + 1 + k))]
+    y = y[y.notnull()]
+    return sum(y) / len(y)
+
+
+
 def read_data(filepath='C:/Users/Alex/Desktop/故障数据.csv'):
     df = pd.read_csv('C:/Users/Alex/Desktop/故障数据.csv', engine='python')
 
@@ -35,7 +72,13 @@ def judgewrong(df,wrongdata):
     ans = []
     return ans
 
-df = read_data()
+df = read_mysql()
+print(df.describe())
+for i in df.columns:
+    for j in range(len(df)):
+        if (df[i].isnull())[j]:
+            df[i][j] = meaninterp_column(df[i], j)
+
 df = dftolist(df)
 wrongdata = getwrong(df)
 wrongdata_judged=judgewrong(df,wrongdata)
